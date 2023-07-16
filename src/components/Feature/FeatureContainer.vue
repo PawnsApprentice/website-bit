68aec28bec386ea17bf06c568b5e6119d65ebcd0
Hassam Abdullah <hassamabdullah1@gmail.com>
2023-07-17 02:15:51 +0500

    link fixed

--- src/components/Feature/FeatureContainer.vue
+++ src/components/Feature/FeatureContainer.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="flex justify-between">
     <FeatureCard
-      imageSrc="%PUBLIC_URL%/assets/physics.png"
+      :imageSrc="physicsImage"
       imageAlt="Physics"
       title="Looking to Migrate from Oracle database?"
       description="We offer enterprise-grade relational database solution, which ensures efficient data management with no compromise on performance."
     />
     <FeatureCard
-      imageSrc="/src/assets/speed.png"
+      :imageSrc="speedImage"
       imageAlt="Speed"
       title="Need Advanced Data Analytics and Insights?"
       description="Tap into your data's full potential with AgeDB's advanced analytics capabilities. Gain deep insights and make data-driven decisions effortlessly."
     />
     <FeatureCard
-      imageSrc="/src/assets/medal.png"
+      :imageSrc="medalImage"
       imageAlt="Medal"
       title="Seeking a Enterprise Postgres Upgrade?"
       description="Experience enhanced performance, scalability, and security with AgeDB's Postgres and AgensSQL solution. Make your data management hassle-free and efficient."
@@ -23,4 +23,7 @@
 
 <script setup>
 import FeatureCard from "./FeatureCard.vue";
+import physicsImage from "../../assets/physics.png";
+import speedImage from "../../assets/speed.png";
+import medalImage from "../../assets/medal.png";
 </script>
