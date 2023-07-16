f049e5a6d216202f96c3c9030e4aaa20abf4582c
Hassam Abdullah <hassamabdullah1@gmail.com>
2023-07-17 02:05:04 +0500

    link fixed

--- src/components/Feature/FeatureContainer.vue
+++ src/components/Feature/FeatureContainer.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="flex justify-between">
     <FeatureCard
-      imageSrc="../../assets/physics.png"
+      imageSrc="%PUBLIC_URL%/assets/physics.png"
       imageAlt="Physics"
       title="Looking to Migrate from Oracle database?"
       description="We offer enterprise-grade relational database solution, which ensures efficient data management with no compromise on performance."
     />
     <FeatureCard
-      imageSrc="../../assets/speed.png"
+      imageSrc="/src/assets/speed.png"
       imageAlt="Speed"
       title="Need Advanced Data Analytics and Insights?"
       description="Tap into your data's full potential with AgeDB's advanced analytics capabilities. Gain deep insights and make data-driven decisions effortlessly."
     />
     <FeatureCard
-      imageSrc="../../assets/medal.png"
+      imageSrc="/src/assets/medal.png"
       imageAlt="Medal"
       title="Seeking a Enterprise Postgres Upgrade?"
       description="Experience enhanced performance, scalability, and security with AgeDB's Postgres and AgensSQL solution. Make your data management hassle-free and efficient."
